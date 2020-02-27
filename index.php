8f8272b6b6d27fbd3c52431c85119936fc77f6e3
arvinkent18 <arvinkent121816@gmail.com>
2020-02-27 16:06:18 +0800

    finished main content section

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
-<div class="container">
-    <div class="grid">
-        <?php get_template_part('template-parts/content', get_post_format()); ?>
+<div id="main-content">
+    <div class="container d-flex h-100">
+        <div class="grid">
+            <?php get_template_part('template-parts/content', get_post_format()); ?>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>

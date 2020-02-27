07e4dba851a115238e35cab64ff7a132d4321fd6
arvinkent18 <arvinkent121816@gmail.com>
2020-02-27 14:29:13 +0800

    initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container-fluid">
+        <div class="grid w-100">
+
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="grid">
+        <?php get_template_part('template-parts/content', get_post_format()); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
